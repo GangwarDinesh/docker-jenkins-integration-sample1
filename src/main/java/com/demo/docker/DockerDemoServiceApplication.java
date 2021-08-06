b459d3a67475dddf6fe580a8ff3e25238ae87b3b
Dinesh Kumar <dineshgngwr@gmail.com>
2021-08-06 16:12:18 +0530

    Update DockerDemoServiceApplication.java
    my sixth commit

--- src/main/java/com/demo/docker/DockerDemoServiceApplication.java
+++ src/main/java/com/demo/docker/DockerDemoServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DockerDemoServiceApplication {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DockerDemoServiceApplication.class, args);
 	}
